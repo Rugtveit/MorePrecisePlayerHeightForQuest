ba4814f532021eccc891f23133ff3604f68b5ee0
Rugtveit <BenjaminRugtveit@gmail.com>
2020-07-01 09:20:39 +0200

    Update Android.mk, bmbfmod.json
    Updated them to support newest version of bs-hook

--- Android.mk
+++ Android.mk
@@ -31,21 +31,23 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE	        := beatsaber-hook_2019_3_0f6_0_1_0
-LOCAL_SRC_FILES         := ./extern/beatsaber-hook/libs/arm64-v8a/libbeatsaber-hook_2019_3_0f6_0_1_0.so
+LOCAL_MODULE	        := beatsaber-hook_2019_3_0f6_0_2_1
+LOCAL_SRC_FILES         := ./extern/beatsaber-hook/libs/arm64-v8a/libbeatsaber-hook_2019_3_0f6_0_2_1.so
 LOCAL_EXPORT_C_INCLUDES := ./extern/beatsaber-hook/
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 # Include the two libraries
 LOCAL_SHARED_LIBRARIES := modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_2019_3_0f6_0_1_0
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2019_3_0f6_0_2_1
 LOCAL_LDLIBS     := -llog
 LOCAL_CFLAGS     := -I"C:\Program Files\Unity\Hub\Editor\2019.3.0f6\Editor\Data\il2cpp\libil2cpp"
 LOCAL_MODULE     := preciseplayerheight
 LOCAL_CPPFLAGS   := -std=c++2a
 LOCAL_C_INCLUDES := ./include ./src
 LOCAL_SRC_FILES  += $(call rwildcard,src/,*.cpp)
+LOCAL_SRC_FILES  += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
+LOCAL_SRC_FILES  += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 include $(BUILD_SHARED_LIBRARY)
 
 # In order to make this mod work with BMBF, you must provide a zip file with the specific libbeatsaber-hook.so (file copied to the libs directory)
