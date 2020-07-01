6b4f890c065801ef9a2b85bdc96031e47ff03b2d
Rugtveit <BenjaminRugtveit@gmail.com>
2020-07-01 09:14:27 +0200

    Update main.cpp, bmbfmod.json
    Updated the mod to be working the same as latest pc version.

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,7 @@
 #include "../extern/beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "../extern/beatsaber-hook/shared/config/config-utils.hpp"
 
-
-
-
 static ModInfo modInfo;
-
 
 static Configuration& getConfig() {
     static Configuration config(modInfo);
@@ -25,26 +21,46 @@ const Logger& getLogger() {
   return logger;
 }
 
+static struct Config_t 
+{
+    bool enabled = true;
+    bool measureInFeet = false; 
+} Config; 
 
-std::string format = "{0:0.00}m";
-Il2CppObject* _playerSettings = nullptr; 
+Il2CppObject* playerSettings = nullptr; 
 float playerHeight = 0.0f;
-Il2CppObject* _text = nullptr; 
+Il2CppObject* heightText = nullptr; 
 
 MAKE_HOOK_OFFSETLESS(PlayerHeightSettingsController_RefreshUI, void, Il2CppObject* self)
 {
-    _playerSettings = CRASH_UNLESS(il2cpp_utils::GetFieldValue(self, "_playerSettings"));
-    _text = CRASH_UNLESS(il2cpp_utils::GetFieldValue(self, "_text"));
-    playerHeight = CRASH_UNLESS(il2cpp_utils::GetPropertyValue<float>(_playerSettings, "playerHeight"));
-    
-    std::stringstream stream;
-    stream << std::fixed << std::setprecision(2) << playerHeight;
-    std::string playerHeightFloat = stream.str();
-    std::string preciseHeightString = playerHeightFloat + "m";
-    
-    CRASH_UNLESS(il2cpp_utils::SetPropertyValue(_text, "text", il2cpp_utils::createcsstr(preciseHeightString)));
+    if(Config.enabled) 
+    {
+        playerSettings = CRASH_UNLESS(il2cpp_utils::GetFieldValue(self, "_playerSettings"));
+        heightText = CRASH_UNLESS(il2cpp_utils::GetFieldValue(self, "_text"));
+        playerHeight = CRASH_UNLESS(il2cpp_utils::GetPropertyValue<float>(playerSettings, "playerHeight"));
+        std::string heightString; 
+        
+        if(Config.measureInFeet)
+        {
+            float inchFeet = playerHeight / 0.3048;
+            int wholeFeet = (int) inchFeet;
+            float inches = round((inchFeet - wholeFeet) / 0.0833);
+            std::stringstream stream;
+            stream << std::fixed << std::setprecision(1) << inches;
+            std::string precisionInchesFloat = stream.str();
+            heightString = std::to_string(wholeFeet) + "\n" + precisionInchesFloat;
+        } else 
+        {
+            std::stringstream stream;
+            stream << std::fixed << std::setprecision(2) << playerHeight;
+            std::string precisionHeightFloat = stream.str();
+            heightString = precisionHeightFloat + "m";
+        }
+        
+        CRASH_UNLESS(il2cpp_utils::SetPropertyValue(heightText, "text", il2cpp_utils::createcsstr(heightString)));
+    } 
+    else PlayerHeightSettingsController_RefreshUI(self); 
 }
-
 
 extern "C" void setup(ModInfo &info)
 {
@@ -56,12 +72,33 @@ extern "C" void setup(ModInfo &info)
     getLogger().info("Modloader name: %s", Modloader::getInfo().name.c_str());
 }  
 
-
+bool LoadConfig();
+void SaveConfig();
 // This function is called when the mod is loaded for the first time, immediately after il2cpp_init.
 extern "C" void load()
 {
+    if(!LoadConfig()) SaveConfig();
     getLogger().debug("Installing MorePrecisePlayerHeight!");
     INSTALL_HOOK_OFFSETLESS(PlayerHeightSettingsController_RefreshUI, il2cpp_utils::FindMethodUnsafe("", "PlayerHeightSettingsController", "RefreshUI", 0));
     getLogger().debug("Installed MorePrecisePlayerHeight!");
 }
 
+void SaveConfig()
+{
+    getConfig().config.RemoveAllMembers();
+    getConfig().config.SetObject();
+    auto& allocator = getConfig().config.GetAllocator();
+    getConfig().config.AddMember("enabled", Config.enabled, allocator);
+    getConfig().config.AddMember("measureInFeet", Config.measureInFeet, allocator);
+    getConfig().Write();
+}
+
+bool LoadConfig()
+{
+    getConfig().Load();
+    if(getConfig().config.HasMember("enabled") && getConfig().config["enabled"].IsBool()) Config.enabled = getConfig().config["enabled"].GetBool(); 
+    else return false;
+    if(getConfig().config.HasMember("measureInFeet") && getConfig().config["measureInFeet"].IsBool()) Config.measureInFeet = getConfig().config["measureInFeet"].GetBool(); 
+    else return false;
+    return true;
+}
